3bf4962ffc1654e2adc9b87c5db414f73280461c
hushuijun <hushuijun@pinkitec.com>
2018-05-07 15:01:13 +0800

    更新代码

--- dome/src/app/app.module.ts
+++ dome/src/app/app.module.ts
@@ -1,22 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // 注入ngModel
+import { HttpClientModule }    from '@angular/common/http';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 
 import { AppComponent } from './app.component';
 import { HeroesMComponent } from './heroes-m/heroes-m.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { HeroSearchComponent }  from './hero-search/hero-search.component';
 
+//服务
+import { HeroService } from './hero.service';
+import { MessageService } from './message.service';
+import { AppRoutingModule } from './/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeroesMComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
-  providers: [],
+  providers: [
+    HeroService,
+    MessageService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- dome/src/app/heroes-m/heroes-m.component.ts
+++ dome/src/app/heroes-m/heroes-m.component.ts
@@ -1,21 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../Hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes-m',
   templateUrl: './heroes-m.component.html',
   styleUrls: ['./heroes-m.component.css']
 })
 export class HeroesMComponent implements OnInit {
-  heroes = HEROES;
-
-  selectedHero: Hero;
-
-  constructor() { }
+  heroes: Hero[];
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
   }
 }
